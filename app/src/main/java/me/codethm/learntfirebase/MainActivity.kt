4362c68180044fe6ef8344ee19d47b9851ba7d54
Codethm <dong55.102375@gmail.com>
2018-08-13 09:52:06 +0700

    wake up

--- app/src/main/java/me/codethm/learntfirebase/MainActivity.kt
+++ app/src/main/java/me/codethm/learntfirebase/MainActivity.kt
@@ -2,11 +2,45 @@ package me.codethm.learntfirebase
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+    private var mAuthListener: FirebaseAuth.AuthStateListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        mAuthListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user = firebaseAuth.currentUser
+            if (user != null) {
+                // User is signed in
+            } else {
+                // User is signed out
+            }
+            // ...
+        }
+        // ...
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        mAuth?.addAuthStateListener(mAuthListener!!)
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        if (mAuthListener != null) {
+            mAuth?.removeAuthStateListener(mAuthListener!!)
+        }
     }
 }
